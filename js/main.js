ddba4a635ef19ca3570ed3b489dde23a7df74668
Felipe <cataofelipe@gmail.com>
2023-03-22 23:13:20 -0300

    create prototipe scene

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ import Oct8Events from "../Oct8-alpha/Oct8/Oct8Events.js"
 
 class Personagens{
    personagens = {
-    aliados:[["Heroi","../img/image832.png"],"Mago"]
+    aliados:[["Heroi","../img/image832.png",10],"Mago"]
+   }
+   inimigos= {
+    inimigos_base:[["blob","../img/Enemyt.png",4]]
    }
 }
-
+ 
 class Game{
     oct_Object = new Oct8()
     prop_element = {
@@ -37,11 +40,24 @@ function CreateChar(persogem){
    char.style.backgroundImage = "url('"+persogem[1]+"')"
    char.style.backgroundSize = "100%"
 
+   var lifebar = OctElem.CreateContainerElement(persogem[0]+"barraVida","conainer-base","barra-vida","div")
+   OctElem.ModifyPropsDefault(lifebar,[50],[1],[(persogem[2]*2)],[4])
+}
+
+function genrateEnemy(inimigo){
+   var enemy = OctElem.CreateContainerElement(inimigo[0],"conainer-base","animateChar","div")
+   OctElem.ModifyPropsDefault(enemy,[120],[-50],[20],[20])
+   enemy.style.backgroundImage = "url('"+inimigo[1]+"')"
+   enemy.style.backgroundSize = "100%"
+
+   var lifebar = OctElem.CreateContainerElement(inimigo[0]+"barraVida","conainer-base","barra-vida","div")
+   OctElem.ModifyPropsDefault(lifebar,[124],[1],[(inimigo[2]*2)],[4])
 }
 
 OctElem.CreateObjectFactory(CreateChar,"Personagem")
+OctElem.CreateObjectFactory(genrateEnemy,"Inimigo")
 OctElem.AppendObjectFacyotyTo("Personagem",Chars.personagens.aliados[0])
-
+OctElem.AppendObjectFacyotyTo("Inimigo",Chars.inimigos.inimigos_base[0])
 
 let game_world = new Game()
 game_world.main()
